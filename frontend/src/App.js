3bde6f6bedcf25ecbc7665b416fbc63cdbfc19ac
LaiThaiThuan1411 <thuanlt9923@ut.edu.vn>
2025-11-19 18:07:31 +0700

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import CheckoutPage from './pages/CheckoutPage';
+import PaymentResultPage from './pages/PaymentResultPage';
+
+function App() {
+  return (
+    <Router>
+      <div style={{ minHeight: '100vh', backgroundColor: '#f5f5f5' }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductDetailPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/payment-result" element={<PaymentResultPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
